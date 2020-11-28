07a1b5337f4d3cbab9d9d2105f25b9ef7d38fa6a
Sophanorin HOEU <heousopharin168@gmail.com>
2020-11-28 18:56:56 +0700

    hosting

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,6 @@ app.get("/", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../num__store/build"));
-
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "num__store", "build", "index.html"));
   });
